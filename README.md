f42751dd8ac47f40a313d59e39898ca3e93cef92
Brett Beatty <brettbeatty@gmail.com>
2024-04-25 02:03:56 -0600

    Support cross-enumerable traces

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 Builds Mermaid sequence diagrams showing what goes on under the hood for Elixir enumerables.
 
-Not entirely clear picture because it forces use of [c:Enumerable.reduce/3](https://hexdocs.pm/elixir/Enumerable.html#reduce/3) even when enumerable is a list or implements another, more case-efficient Enumerable callback.
-
 ## Installation
 
 EnumerableTracer is intended to be run in Livebook. It can be installed using [Mix.install/2](https://hexdocs.pm/mix/Mix.html#install/2).
@@ -14,78 +12,84 @@ Mix.install([{:enumerable_tracer, github: "brettbeatty/elixir_enumerable_tracer"
 
 ## Usage
 
-EnumerableTracer wraps enumerables in a stream that renders a Mermaid diagram using [Kino.render/1](https://hexdocs.pm/kino/Kino.html#render/1) upon completion.
-
-This applies both when the underlying enumerable reaches its end and when it's halted early.
+EnumerableTracer wraps enumerables in a stream that traces their implementation of `Enumerable`. These streams must be run in the function passed to `EnumerableTracer.run/2`, which renders a Mermaid diagram upon completion.
 
 Any easy way to see the underlying enumerable iterated in its entirety is using `Enum.to_list/1`.
 
 ```elixir
-1..5 |> EnumerableTracer.trace() |> Enum.to_list()
+enumerable = EnumerableTracer.trace(1..3, label: "1..3")
+
+EnumerableTracer.run(fn ->
+  Enum.to_list(enumerable)
+end)
 ```
 
 ```mermaid
 sequenceDiagram
- caller->>enumerable: Enumerable.reduce(enumerable, {:cont, []}, reducer)
- enumerable->>reducer: reducer.(1, [])
- reducer->>enumerable: {:cont, [1]}
- enumerable->>reducer: reducer.(2, [1])
- reducer->>enumerable: {:cont, [2, 1]}
- enumerable->>reducer: reducer.(3, [2, 1])
- reducer->>enumerable: {:cont, [3, 2, 1]}
- enumerable->>reducer: reducer.(4, [3, 2, 1])
- reducer->>enumerable: {:cont, [4, 3, 2, 1]}
- enumerable->>reducer: reducer.(5, [4, 3, 2, 1])
- reducer->>enumerable: {:cont, [5, 4, 3, 2, 1]}
- enumerable->>caller: {:done, [5, 4, 3, 2, 1]}
+  participant HBSRCQCE as root
+  participant G5HOGDJ5 as 1..3
+  participant UYY5LA34 as reducer_UYY5LA34
+  HBSRCQCE->>G5HOGDJ5: Enumerable.reduce(1..3, {:cont, []}, reducer_UYY5LA34)
+  G5HOGDJ5->>UYY5LA34: reducer_UYY5LA34.(1, [])
+  UYY5LA34->>G5HOGDJ5: {:cont, [1]}
+  G5HOGDJ5->>UYY5LA34: reducer_UYY5LA34.(2, [1])
+  UYY5LA34->>G5HOGDJ5: {:cont, [2, 1]}
+  G5HOGDJ5->>UYY5LA34: reducer_UYY5LA34.(3, [2, 1])
+  UYY5LA34->>G5HOGDJ5: {:cont, [3, 2, 1]}
+  G5HOGDJ5->>HBSRCQCE: {:done, [3, 2, 1]}
 ```
 
 ```
-[1, 2, 3, 4, 5]
+[1, 2, 3]
 ```
 
 For both suspending and halting the enumerable, you can use `Enum.zip/2` to zip the enumerable with another, shorter one.
 
 ```elixir
-[:a, :b, :c, :d, :e] |> EnumerableTracer.trace() |> Enum.zip(6..9)
+enumerable = EnumerableTracer.trace([:a, :b, :c, :d, :e])
+
+EnumerableTracer.run(fn ->
+  Enum.zip(enumerable, 4..7)
+end)
 ```
 
 ```mermaid
 sequenceDiagram
- caller->>enumerable: Enumerable.reduce(enumerable, {:cont, []}, reducer)
- enumerable->>reducer: reducer.(:a, [])
- reducer->>enumerable: {:suspend, [:a]}
- enumerable->>caller: {:suspended, [:a], continuation1}
- caller->>enumerable: continuation1.({:cont, []})
- enumerable->>reducer: reducer.(:b, [])
- reducer->>enumerable: {:suspend, [:b]}
- enumerable->>caller: {:suspended, [:b], continuation2}
- caller->>enumerable: continuation2.({:cont, []})
- enumerable->>reducer: reducer.(:c, [])
- reducer->>enumerable: {:suspend, [:c]}
- enumerable->>caller: {:suspended, [:c], continuation3}
- caller->>enumerable: continuation3.({:cont, []})
- enumerable->>reducer: reducer.(:d, [])
- reducer->>enumerable: {:suspend, [:d]}
- enumerable->>caller: {:suspended, [:d], continuation4}
- caller->>enumerable: continuation4.({:cont, []})
- enumerable->>reducer: reducer.(:e, [])
- reducer->>enumerable: {:suspend, [:e]}
- enumerable->>caller: {:suspended, [:e], continuation5}
- caller->>enumerable: continuation5.({:halt, []})
- enumerable->>caller: {:halted, []}
+  participant L7V33XOK as root
+  participant NVRGWAGA as enumerable_NVRGWAGA
+  participant KTS5HDEU as reducer_KTS5HDEU
+  L7V33XOK->>NVRGWAGA: Enumerable.reduce(enumerable_NVRGWAGA, {:cont, []}, reducer_KTS5HDEU)
+  NVRGWAGA->>KTS5HDEU: reducer_KTS5HDEU.(:a, [])
+  KTS5HDEU->>NVRGWAGA: {:suspend, [:a]}
+  NVRGWAGA->>L7V33XOK: {:suspended, [:a], continuation}
+  L7V33XOK->>NVRGWAGA: continuation.({:cont, []})
+  NVRGWAGA->>KTS5HDEU: reducer_KTS5HDEU.(:b, [])
+  KTS5HDEU->>NVRGWAGA: {:suspend, [:b]}
+  NVRGWAGA->>L7V33XOK: {:suspended, [:b], continuation}
+  L7V33XOK->>NVRGWAGA: continuation.({:cont, []})
+  NVRGWAGA->>KTS5HDEU: reducer_KTS5HDEU.(:c, [])
+  KTS5HDEU->>NVRGWAGA: {:suspend, [:c]}
+  NVRGWAGA->>L7V33XOK: {:suspended, [:c], continuation}
+  L7V33XOK->>NVRGWAGA: continuation.({:cont, []})
+  NVRGWAGA->>KTS5HDEU: reducer_KTS5HDEU.(:d, [])
+  KTS5HDEU->>NVRGWAGA: {:suspend, [:d]}
+  NVRGWAGA->>L7V33XOK: {:suspended, [:d], continuation}
+  L7V33XOK->>NVRGWAGA: continuation.({:cont, []})
+  NVRGWAGA->>KTS5HDEU: reducer_KTS5HDEU.(:e, [])
+  KTS5HDEU->>NVRGWAGA: {:suspend, [:e]}
+  NVRGWAGA->>L7V33XOK: {:suspended, [:e], continuation}
+  L7V33XOK->>NVRGWAGA: continuation.({:halt, []})
+  NVRGWAGA->>L7V33XOK: {:halted, []}
 ```
 
 ```
-[a: 6, b: 7, c: 8, d: 9]
+[a: 4, b: 5, c: 6, d: 7]
 ```
 
 ## Shortcomings
 
-This is just an MVP and has some serious flaws.
+This is still very much in a hacked-together state.
 
 It doesn't do anything to escape values rendered in Mermaid, so any inspected values with special characters for Mermaid could break the diagram. For example, any values that inspect with a "#" prefix will get cut off.
 
-Traces are also tied in a 1:1 relationship with enumerables. You can't have more than one enumerable in a trace. If you zip together two traced enumerables, for example, you'll see one diagram then another--they won't be intertwined. It could be interesting to have an async trace that sends trace messages to the process inbox and has an explicit gather/render function.
-
-Also the names for the "participants" in the sequence diagram are static ("caller", "enumerable", and "reducer"). It could be interesting and probably fairly simple to let you pass in what to call each of them.
+Also the names of reducers cannot be altered.
--- lib/enumerable_tracer.ex
+++ lib/enumerable_tracer.ex
@@ -1,70 +1,402 @@
 defmodule EnumerableTracer do
-  def trace(enumerable) do
-    fn cmd_acc, reducer ->
-      traced_reducer = fn element, {traces, acc} ->
-        {new_cmd, new_acc} = reducer.(element, acc)
+  @type label() :: iodata()
+  @type t() :: %__MODULE__{enumerable: Enumerable.t(), label: label(), tag: tag()}
+  @type tag() :: <<_::64>>
 
-        call_trace =
-          {"enumerable", "reducer", ["reducer.(", inspect(element), ", ", inspect(acc), ")"]}
+  @typep label_builder() :: {[tag()], ([label()] -> label())}
+  @typep state() :: %{caller: tag(), labels: %{tag() => label()}, traces: [trace()]}
+  @typep trace() :: {tag(), tag(), label_builder()}
 
-        result_trace = {"reducer", "enumerable", inspect({new_cmd, new_acc})}
-        {new_cmd, {[result_trace, call_trace | traces], new_acc}}
-      end
+  defstruct [:enumerable, :label, :tag]
 
-      continuation = &Enumerable.reduce(enumerable, &1, traced_reducer)
-      continue(cmd_acc, continuation, _traces = [], _counter = 0)
+  @spec run((-> result)) :: result when result: var
+  @spec run(label(), (-> result)) :: result when result: var
+  def run(root_label \\ "root", fun) do
+    tag = build_tag()
+    old_state = put_state(%{caller: tag, labels: %{tag => root_label}, traces: []})
+
+    try do
+      result = fun.()
+
+      get_state() |> print()
+
+      result
+    after
+      put_state(old_state)
     end
   end
 
-  defp continue(cmd_acc, continuation, traces, counter) do
-    call_label =
-      if counter > 0 do
-        ["continuation", to_string(counter), ".(", inspect(cmd_acc), ")"]
-      else
-        ["Enumerable.reduce(enumerable, ", inspect(cmd_acc), ", reducer)"]
-      end
-
-    call_trace = {"caller", "enumerable", call_label}
-    {cmd, acc} = cmd_acc
-
-    case continuation.({cmd, {[call_trace | traces], acc}}) do
-      {result_type, {new_traces, new_acc}} when result_type in [:done, :halted] ->
-        result = {result_type, new_acc}
-        result_trace = {"enumerable", "caller", inspect(result)}
-        print_trace([result_trace | new_traces])
-        result
-
-      {:suspended, {new_traces, new_acc}, new_continuation} ->
-        result_label = [
-          "{:suspended, ",
-          inspect(new_acc),
-          ", continuation",
-          to_string(counter + 1),
-          "}"
-        ]
-
-        result_trace = {"enumerable", "caller", result_label}
-        newer_traces = [result_trace | new_traces]
-        {:suspended, new_acc, &continue(&1, new_continuation, newer_traces, counter + 1)}
-    end
+  @spec build_tag() :: tag()
+  defp build_tag do
+    5
+    |> :crypto.strong_rand_bytes()
+    |> Base.encode32()
   end
 
-  defp print_trace(traces) do
-    traces
-    |> build_diagram([])
-    |> to_string()
+  @spec put_state(state()) :: state() | nil
+  defp put_state(state), do: Process.put(__MODULE__, state)
+
+  @spec get_state() :: state() | nil
+  defp get_state, do: Process.get(__MODULE__)
+
+  @spec print(state()) :: :ok
+  defp print(state) do
+    %{labels: labels, traces: reversed_traces} = state
+    fetch_label = &Map.fetch!(labels, &1)
+    traces = Enum.reverse(reversed_traces)
+
+    diagram_lines = compile_diagram(traces, fetch_label)
+
+    render_tabs(diagram: build_diagram(diagram_lines), raw: build_raw(diagram_lines))
+  end
+
+  @spec compile_diagram([trace()], (tag() -> label())) :: [iodata()]
+  defp compile_diagram(traces, fetch_label) do
+    participant_lines =
+      traces
+      |> Stream.flat_map(fn {from, to, _label_builder} -> [from, to] end)
+      |> Stream.uniq()
+      |> Stream.map(&["participant ", &1, " as ", fetch_label.(&1)])
+
+    trace_lines =
+      Stream.map(traces, fn {from, to, label_builder} ->
+        [from, "->>", to, ": " | build_label(label_builder, fetch_label)]
+      end)
+
+    indented_lines =
+      participant_lines
+      |> Stream.concat(trace_lines)
+      |> Enum.map(&["  " | &1])
+
+    ["sequenceDiagram" | indented_lines]
+  end
+
+  @spec build_label(label_builder(), (tag() -> label())) :: label()
+  defp build_label({tags, build}, fetch_label), do: tags |> Enum.map(fetch_label) |> build.()
+
+  @spec build_diagram([iodata()]) :: Kino.Render.t()
+  defp build_diagram(lines) do
+    lines
+    |> Enum.join("\n")
     |> Kino.Mermaid.new()
+  end
+
+  @spec build_raw([iodata()]) :: Kino.Render.t()
+  defp build_raw(lines) do
+    lines
+    |> Enum.join("\n    ")
+    |> then(&"    ```mermaid\n    #{&1}\n    ```")
+    |> Kino.Markdown.new()
+  end
+
+  @spec render_tabs(keyword(Kino.Render.t())) :: :ok
+  defp render_tabs(tabs) do
+    tabs
+    |> Kino.Layout.tabs()
     |> Kino.render()
+
+    :ok
   end
 
-  defp build_diagram(traces, acc)
+  @spec trace(Enumerable.t()) :: t()
+  @spec trace(Enumerable.t(), label: iodata()) :: t()
+  def trace(enumerable, opts \\ []) do
+    tag = build_tag()
 
-  defp build_diagram([trace | traces], acc) do
-    {from, to, label} = trace
-    build_diagram(traces, ["\n ", from, "->>", to, ": ", label | acc])
+    %__MODULE__{
+      enumerable: enumerable,
+      label: Keyword.get(opts, :label, ["enumerable_", tag]),
+      tag: tag
+    }
   end
 
-  defp build_diagram([], acc) do
-    ["sequenceDiagram" | acc]
+  @doc false
+  @spec _trace_count(t()) :: {:ok, non_neg_integer()} | {:error, module()}
+  def _trace_count(tracer) do
+    register_label(tracer)
+
+    with_caller(tracer.tag, fn caller, callee ->
+      put_trace(caller, callee, count_request(tracer))
+      result = Enumerable.count(tracer.enumerable)
+      put_trace(callee, caller, inspect_response(result))
+
+      with {:error, _module} <- result do
+        {:error, Enumerable.EnumerableTracer}
+      end
+    end)
+  end
+
+  @spec count_request(t()) :: label_builder()
+  defp count_request(tracer) do
+    {[tracer.tag], fn [label] -> ["Enumerable.count(", label, ")"] end}
+  end
+
+  @doc false
+  @spec _trace_member(t(), term()) :: {:ok, boolean()} | {:error, module()}
+  def _trace_member(tracer, element) do
+    register_label(tracer)
+
+    with_caller(tracer.tag, fn caller, callee ->
+      put_trace(caller, callee, member_request(tracer, element))
+      result = Enumerable.member?(tracer.enumerable, element)
+      put_trace(callee, caller, inspect_response(result))
+
+      with {:error, _module} <- result do
+        {:error, Enumerable.EnumerableTracer}
+      end
+    end)
+  end
+
+  @spec member_request(t(), term()) :: label_builder()
+  defp member_request(tracer, element) do
+    {[tracer.tag],
+     fn [label] -> ["Enumerable.member?(", label, ", ", iodata_inspect(element), ")"] end}
+  end
+
+  @doc false
+  @spec _trace_reduce(t(), Enumerable.acc(), Enumerable.reducer()) :: Enumerable.result()
+  def _trace_reduce(tracer, acc, fun) do
+    register_label(tracer)
+
+    {reducer_tag, reducer} = trace_reducer(fun)
+    register_label(reducer_tag, ["reducer_", reducer_tag])
+
+    with_caller(tracer.tag, fn caller, callee ->
+      put_trace(caller, callee, reduce_request(tracer, acc, reducer_tag))
+      continuation = &Enumerable.reduce(tracer.enumerable, &1, reducer)
+      reduce(acc, continuation, caller, callee)
+    end)
+  end
+
+  @spec reduce_request(t(), Enumerable.acc(), tag()) :: label_builder()
+  defp reduce_request(tracer, acc, reducer_tag) do
+    {[tracer.tag, reducer_tag],
+     fn [enumerable, reducer] ->
+       ["Enumerable.reduce(", enumerable, ", ", iodata_inspect(acc), ", ", reducer, ")"]
+     end}
+  end
+
+  @spec trace_reducer(Enumerable.reducer()) :: {tag(), Enumerable.reducer()}
+  defp trace_reducer(fun) do
+    tag = build_tag()
+
+    reducer =
+      fn element, acc ->
+        with_caller(tag, fn caller, callee ->
+          put_trace(caller, callee, reducer_request(tag, element, acc))
+          result = fun.(element, acc)
+          put_trace(callee, caller, inspect_response(result))
+          result
+        end)
+      end
+
+    {tag, reducer}
+  end
+
+  @spec reducer_request(tag(), term(), term()) :: label_builder()
+  defp reducer_request(tag, element, acc) do
+    {[tag],
+     fn [label] -> [label, ".(", iodata_inspect(element), ", ", iodata_inspect(acc), ")"] end}
+  end
+
+  @spec reduce(Enumerable.acc(), Enumerable.continuation(), tag(), tag()) :: Enumerable.result()
+  defp reduce(acc, continuation, caller, callee) do
+    result = continuation.(acc)
+    put_trace(callee, caller, reduce_response(result))
+
+    with {:suspended, new_acc, new_continuation} <- result do
+      {:suspended, new_acc, &continue(&1, new_continuation, callee)}
+    end
+  end
+
+  @spec reduce_response(Enumerable.result()) :: label_builder()
+  defp reduce_response(result) do
+    case result do
+      {status, _acc} when status in [:done, :halted] ->
+        inspect_response(result)
+
+      {:suspended, acc, _continuation} ->
+        {[], fn [] -> ["{:suspended, ", iodata_inspect(acc), ", continuation}"] end}
+    end
+  end
+
+  @spec continue(Enumerable.acc(), Enumerable.continuation(), tag()) :: Enumerable.result()
+  defp continue(acc, continuation, tag) do
+    with_caller(tag, fn caller, callee ->
+      put_trace(caller, callee, continue_request(acc))
+      reduce(acc, continuation, caller, callee)
+    end)
+  end
+
+  @spec continue_request(Enumerable.acc()) :: label_builder()
+  defp continue_request(acc) do
+    {[], fn [] -> ["continuation.(", iodata_inspect(acc), ")"] end}
+  end
+
+  @doc false
+  @spec _trace_slice(t()) ::
+          {:ok, non_neg_integer(), Enumerable.slicing_fun() | Enumerable.to_list_fun()}
+          | {:error, module()}
+  def _trace_slice(tracer) do
+    register_label(tracer)
+
+    with_caller(tracer.tag, fn caller, callee ->
+      put_trace(caller, callee, slice_request(tracer))
+      result = Enumerable.slice(tracer.enumerable)
+      put_trace(callee, caller, slice_response(result))
+
+      case result do
+        {:ok, size, slicing_fun} when is_function(slicing_fun, 3) ->
+          {:ok, size, trace_slicing_fun(slicing_fun, tracer.tag)}
+
+        {:ok, size, to_list_fun} when is_function(to_list_fun, 1) ->
+          {:ok, size, trace_to_list_fun(to_list_fun)}
+
+        {:error, _module} ->
+          {:error, Enumerable.EnumerableTracer}
+      end
+    end)
+  end
+
+  @spec slice_request(t()) :: label_builder()
+  defp slice_request(tracer) do
+    {[tracer.tag], fn [label] -> ["Enumerable.slice(", label, ")"] end}
+  end
+
+  @spec slice_response(
+          {:ok, size :: non_neg_integer(), Enumerable.slicing_fun() | Enumerable.to_list_fun()}
+          | {:error, module()}
+        ) :: label_builder()
+  defp slice_response(result) do
+    case result do
+      {:ok, size, slicing_fun} when is_function(slicing_fun, 3) ->
+        {[], fn [] -> ["{:ok, ", to_string(size), ", slicing_fun}"] end}
+
+      {:ok, size, to_list_fun} when is_function(to_list_fun, 1) ->
+        {[], fn [] -> ["{:ok, ", to_string(size), ", to_list_fun}"] end}
+
+      {:error, _module} ->
+        inspect_response(result)
+    end
+  end
+
+  @spec trace_slicing_fun(Enumerable.slicing_fun(), tag()) :: Enumerable.slicing_fun()
+  defp trace_slicing_fun(slicing_fun, tag) do
+    fn start, length, step ->
+      with_caller(tag, fn caller, callee ->
+        put_trace(caller, callee, slicing_fun_request(start, length, step))
+        result = slicing_fun.(start, length, step)
+        put_trace(caller, callee, inspect_response(result))
+        result
+      end)
+    end
+  end
+
+  @spec slicing_fun_request(non_neg_integer(), pos_integer(), pos_integer()) :: label_builder()
+  defp slicing_fun_request(start, length, step) do
+    {[],
+     fn [] ->
+       ["slicing_fun.(", to_string(start), ", ", to_string(length), ", ", to_string(step), ")"]
+     end}
+  end
+
+  @spec trace_to_list_fun(Enumerable.to_list_fun()) :: Enumerable.to_list_fun()
+  defp trace_to_list_fun(to_list_fun) do
+    fn tracer ->
+      with_caller(tracer.tag, fn caller, callee ->
+        put_trace(caller, callee, to_list_fun_request(tracer.tag))
+        result = to_list_fun.(tracer.enumerable)
+        put_trace(callee, caller, inspect_response(result))
+        result
+      end)
+    end
+  end
+
+  @spec to_list_fun_request(tag()) :: label_builder()
+  defp to_list_fun_request(tag) do
+    {[tag], fn [label] -> ["to_list_fun.(", label, ")"] end}
+  end
+
+  @spec register_label(t()) :: :ok
+  defp register_label(tracer) do
+    register_label(tracer.tag, tracer.label)
+  end
+
+  @spec register_label(tag(), label()) :: :ok
+  defp register_label(tag, label) do
+    update_state(fn state ->
+      Map.update!(state, :labels, &Map.put(&1, tag, label))
+    end)
+  end
+
+  @spec with_caller(tag(), (tag(), tag() -> result)) :: result when result: var
+  defp with_caller(callee, fun) do
+    caller = get_state().caller
+    update_state(&Map.put(&1, :caller, callee))
+
+    try do
+      fun.(caller, callee)
+    after
+      update_state(&Map.put(&1, :caller, caller))
+    end
+  end
+
+  @spec put_trace(tag(), tag(), label_builder()) :: :ok
+  defp put_trace(from, to, label_builder) do
+    update_state(fn state ->
+      Map.update!(state, :traces, &[{from, to, label_builder} | &1])
+    end)
+  end
+
+  @spec update_state((state() -> state())) :: :ok
+  defp update_state(fun) do
+    get_state()
+    |> fun.()
+    |> then(&put_state/1)
+
+    :ok
+  end
+
+  @spec inspect_response(any()) :: label_builder()
+  defp inspect_response(term) do
+    {[], fn [] -> iodata_inspect(term) end}
+  end
+
+  @spec iodata_inspect(Inspect.t()) :: iodata()
+  defp iodata_inspect(term) do
+    opts = Inspect.Opts.new([])
+
+    limit =
+      case opts.pretty do
+        true -> opts.width
+        false -> :infinity
+      end
+
+    term
+    |> Inspect.Algebra.to_doc(opts)
+    |> Inspect.Algebra.group()
+    |> Inspect.Algebra.format(limit)
+  end
+
+  defimpl Enumerable do
+    @impl Enumerable
+    def count(tracer) do
+      EnumerableTracer._trace_count(tracer)
+    end
+
+    @impl Enumerable
+    def member?(tracer, element) do
+      EnumerableTracer._trace_member(tracer, element)
+    end
+
+    @impl Enumerable
+    def reduce(tracer, acc, fun) do
+      EnumerableTracer._trace_reduce(tracer, acc, fun)
+    end
+
+    @impl Enumerable
+    def slice(tracer) do
+      EnumerableTracer._trace_slice(tracer)
+    end
   end
 end
